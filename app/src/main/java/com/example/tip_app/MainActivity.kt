68fa490f93e878f66e771ba9279c2dc2f097b357
steved <smddurbs@gmail.com>
2022-04-02 17:15:18 +1300

    Created Header

--- app/src/main/java/com/example/tip_app/MainActivity.kt
+++ app/src/main/java/com/example/tip_app/MainActivity.kt
@@ -3,45 +3,99 @@ package com.example.tip_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tip_app.ui.theme.Tip_AppTheme
+import java.util.concurrent.ConcurrentLinkedDeque
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Tip_AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp {
+                    TopHeader()
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-    Column() {
-        Text(text = "hello")
+fun MyApp(content: @Composable () -> Unit) {
+    content()
+}
+
+@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(10.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(20.dp))),
+        color = Color(0xFF48a850)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                "$$total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview
+@Composable
+fun MainContent(){
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(1.dp, color = Color(0xFF48a850))
+    ){
+        Column() {
+
+        }
+    }
+}
+
+
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Tip_AppTheme {
-        Greeting("Android")
+        Tip_AppTheme {
+            MyApp {
+                Text("Hello")
+            }
+        }
     }
 }
