38c433b23bbeb3a18d1994a51d60ef4ef9b30787
steved <smddurbs@gmail.com>
2022-04-02 19:11:05 +1300

    Added customisable input field

--- app/src/main/java/com/example/tip_app/MainActivity.kt
+++ app/src/main/java/com/example/tip_app/MainActivity.kt
@@ -7,18 +7,24 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tip_app.components.InputField
 import com.example.tip_app.ui.theme.Tip_AppTheme
 import java.util.concurrent.ConcurrentLinkedDeque
 
@@ -28,7 +34,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tip_AppTheme {
                 MyApp {
-                    TopHeader()
+                    //TopHeader()
+                    MainContent()
 
             }
         }
@@ -71,9 +78,17 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent(){
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value){
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
     Surface(
         modifier = Modifier
             .padding(2.dp)
@@ -82,7 +97,18 @@ fun MainContent(){
         border = BorderStroke(1.dp, color = Color(0xFF48a850))
     ){
         Column() {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Amount",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+                    if(!validState) return@KeyboardActions
+                    //Todo - onvaluechanged
 
+                    keyboardController?.hide()
+                }
+            )
         }
     }
 }
